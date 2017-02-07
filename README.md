da514663cd0d6f698dae5c37c1936a89fd278f1e
Daniel González <d.g.g69@me.com>
2017-02-06 21:43:05 -0600

    Finished Components within components

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Following tutorial from [Learn Vue 2: Step by step](https://laracasts.com/series
 - [X] Attribute and class binding
 - [X] The need for computed properties
 - [X] Components 101
-- [ ] Components within components
+- [X] Components within components
 - [ ] Practical component excercise #1: Message
 - [ ] Practical component excercise #2: Modal
 - [ ] Practical component excercise #3: Tabs
--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+Vue.component('task-list', {
+  data() {
+    return {
+      tasks: [
+        { task: 'Go to the store', complete: true },
+        { task: 'Go to the drugstore', complete: false },
+        { task: 'Go to the farm', complete: true },
+        { task: 'Go to work', complete: false }
+      ]
+    }
+  },
+  template: `
+    <ul>
+      <task v-for="task in tasks">{{ task.task }}</task>
+    </ul>
+  `
+});
+
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 });
